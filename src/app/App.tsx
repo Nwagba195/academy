b749e92600dab1a464790d8d1ced6f93d5e2789e
All_Father <49503150+Nwagba195@users.noreply.github.com>
2023-12-14 00:53:36 +0100

    adjusted layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,11 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import Navbar from "./components/Navbar";
+import Navbar from "../app/components/Navbar";
 import Footer from "./components/Footer";
 
 const App = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Provider store={store}>
-      <Navbar />
-      {children}
-      <Footer />
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
 
 export default App;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import "./globals.css";
 import App from "./App";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <App>{children}</App>
+        <App>
+          <Navbar />
+          {children}
+          <Footer />
+        </App>
       </body>
     </html>
   );
